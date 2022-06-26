336fbb176d4c7992cb22c22a6b234121eee6938d
s-asai <s-asai@ruby-dev.jp>
2022-06-26 21:34:45 +0900

    lockable機能の追加

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,6 @@
 class AdminsController < ApplicationController
+  before_action :authenticate_admin!, only: %i[show]
+
   def show
     @admin = Admin.find(current_admin.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  root "users#index"\
+  root "users#index"
 
   resources :users
-  resources :admins, only: [:show]
 
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
     registrations: 'admins/registrations'
   }
+
+  resources :admins, only: [:show]
 end
